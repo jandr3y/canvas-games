35936e3cce86f5081dc7e2e9fe84b8d1378faf97
Lucas Jandrey <wdev.lucas@gmail.com>
2018-09-30 21:51:33 -0300

    separado js

---
+++ script.js
@@ -0,0 +1,115 @@
+const canvas = document.getElementById("screen")
+      const ctx = canvas.getContext('2d')
+
+      let controlls = {
+        up : false,
+        down : false,
+        left : false,
+        right : false
+      }
+
+      let map = {
+        initX: 900,
+        obsHeight :Math.floor(Math.random() * (80 - 40)) + 40,
+        obsVelocity: 5,
+        obsMaxVelocity: 25,
+        obsWidth: 30,
+        ground : function(){
+          ctx.fillStyle = 'black'
+          ctx.fillRect(0, 440, canvas.width, 60);
+        },
+        obs : function(x){
+          ctx.fillStyle = 'black'
+          ctx.fillRect(x, (440 - this.obsHeight), this.obsWidth, this.obsHeight)
+        },
+        draw : function(){
+          if(this.initX < -20) {
+            this.initX = 900
+            this.obsHeight = Math.floor(Math.random() * (80 - 40)) + 40;
+            if(this.obsVelocity < this.obsMaxVelocity) this.obsVelocity += 1
+          }
+          this.ground()
+          this.obs(this.initX)
+          this.initX -= this.obsVelocity
+        }
+      }
+
+      let player = {
+        x: 50,
+        y: 50,
+        radius: 25,
+        velocity: 10,
+        draw: function(){
+          ctx.fillStyle = 'red'
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.radius,0,2*Math.PI);
+          ctx.fill();
+          ctx.stroke();
+        },
+        movement : function(){
+          if(controlls.up) this.jump.state = true
+          if(controlls.left) {
+            if(this.x > 30) this.x -= this.velocity
+          }
+          // if(controlls.down) this.y += this.velocity
+          if(controlls.right){
+            if(this.x < 700- this.radius) this.x += this.velocity
+          }
+        },
+        jump : {
+          state : false,
+          jumpForce : 20,
+          exec : function(){
+            if(this.jumpForce > 1){
+              this.jumpForce = this.jumpForce - world.gravityValue
+              player.y -= this.jumpForce
+            }else{
+              player.y = 415
+              this.state = false
+            }
+          }
+        }
+      }
+
+      let world = {
+        gravityValue: 0.9,
+        gravitySpeed: 0,
+        gravity: function(){
+          this.gravitySpeed += this.gravityValue
+          player.y += this.gravitySpeed
+        } 
+      }
+
+      function gameloop(){
+        if (player.x + player.radius >= map.initX && player.x - player.radius <= map.initX + map.obsWidth) {
+          if (player.y >= (440 - map.obsHeight - player.radius )) {
+            alert("Você Perdeu!")
+            location.reload(); 
+          }
+        }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        map.draw()
+        if(player.y < 400) world.gravity()
+        else world.gravitySpeed = 0
+        if(player.jump.state) player.jump.exec()
+        else player.jump.jumpForce = 20
+        player.movement()
+        player.draw()
+
+      }
+
+      window.addEventListener("keydown", function(e){
+        if(e.keyCode === 37) controlls.left = true;
+        if(e.keyCode === 38) controlls.up = true;
+        if(e.keyCode === 39) controlls.right = true;
+        if(e.keyCode === 40) controlls.down = true;
+      })
+
+      window.addEventListener("keyup", function(e){
+        if(e.keyCode === 37) controlls.left = false;
+        if(e.keyCode === 38) controlls.up = false;
+        if(e.keyCode === 39) controlls.right = false;
+        if(e.keyCode === 40) controlls.down = false;
+      })
+
+      setInterval(gameloop, 32)
